9084d03b3ccb0dfa978ae37535af02cba0506354
Will <Will.irvin18@gmail.com>
2019-08-16 16:16:37 -0400

    have the score div working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,16 +22,69 @@ $(document).ready(function () {
         
 
         var radioValue = $("input[name='q1']:checked").val();
-        if  (radioValue != "male"){
-            incorrect++;
-        }
-        else if (radioValue === "male") {
+        if (radioValue === "male") {
             correct++;
          }
-         else  {
+        else if (radioValue != "male") {
+            incorrect++;
+        }
+        
+        if (!$("input[name='q1']:checked").val()) {
             unanswered++;
-            
-        }  
+            incorrect--;
+        }
+        
+        var radioValue2 = $("input[name='q2']:checked").val();
+        if (radioValue2 === "male") {
+            correct++;
+         }
+        else if (radioValue2 != "male") {
+            incorrect++;
+        }
+        
+        if (!$("input[name='q2']:checked").val()) {
+            unanswered++;
+            incorrect--;
+        }
+
+        var radioValue3 = $("input[name='q3']:checked").val();
+        if (radioValue3 === "male") {
+            correct++;
+         }
+        else if (radioValue3 != "male") {
+            incorrect++;
+        }
+        
+        if (!$("input[name='q3']:checked").val()) {
+            unanswered++;
+            incorrect--;
+        }
+
+        var radioValue4 = $("input[name='q4']:checked").val();
+        if (radioValue4 === "male") {
+            correct++;
+         }
+        else if (radioValue4 != "male") {
+            incorrect++;
+        }
+        
+        if (!$("input[name='q4']:checked").val()) {
+            unanswered++;
+            incorrect--;
+        }
+
+        var radioValue5 = $("input[name='q5']:checked").val();
+        if (radioValue5 === "male") {
+            correct++;
+         }
+        else if (radioValue5 != "male") {
+            incorrect++;
+        }
+        
+        if (!$("input[name='q5']:checked").val()) {
+            unanswered++;
+            incorrect--;
+        }
         
 
         $("#correct").text("Correct: " + correct);
