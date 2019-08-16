8a34f2dfee883caf54c3de0a56edc918897c3ea1
Will <Will.irvin18@gmail.com>
2019-08-16 15:46:00 -0400

    javascript almost finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,43 @@
-$("#start").click(function () {
-    $("#questions").toggle();
-    $("#start").hide();
+$(document).ready(function () {
+
+
+
+
+    $("#start").click(function () {
+        $("#questions").toggle();
+        $("#start").hide();
+    });
+
+    $("#submit").click(function () {
+        $("#doneContainer").toggle();
+        $("#submit").hide();
+        $("#questions").toggle();
+    });
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+
+    $("#submit").click(function () {
+        
+
+        var radioValue = $("input[name='q1']:checked").val();
+        if  (radioValue != "male"){
+            incorrect++;
+        }
+        else if (radioValue === "male") {
+            correct++;
+         }
+         else  {
+            unanswered++;
+            
+        }  
+        
+
+        $("#correct").text("Correct: " + correct);
+        $("#incorrect").text("Incorrect: " + incorrect);
+        $("#unanswered").text("Unanswered: " + unanswered);
+    });
+
 });
-
-$("#submit").click(function () {
-    $("#doneContainer").toggle();
-    $("#submit").hide();
-    $("#questions").toggle();
-});
-
-
 
